99af78d2069f425a2b176b9843c945ebb6ebd75f
Emmytech12 <seunemmanuel107@gmail.com>
2026-01-08 20:15:17 +0100

    update: update 2 @08-01-2026 on Mota Odumusi Api Configuration

--- api/dev/config/connection.php
+++ api/dev/config/connection.php
@@ -6,13 +6,13 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header('Content-Type: application/json; charset=UTF-8');
 
 ////////////for live connect  
-// $_HOST_NAME = "145.239.185.59";  
-// $_DB_USERNAME ="peakan";
-// $_DB_PASSWORD ="Password@12345!";
+$_HOST_NAME = "152.53.89.36";  
+$_DB_USERNAME ="motaodumusi";
+$_DB_PASSWORD ="Password@Dec312025";
 
-$_HOST_NAME = "localhost";  
-$_DB_USERNAME ="root";
-$_DB_PASSWORD ="";
+// $_HOST_NAME = "localhost";  
+// $_DB_USERNAME ="root";
+// $_DB_PASSWORD ="";
 $conn = mysqli_connect($_HOST_NAME, $_DB_USERNAME, $_DB_PASSWORD)or die("Unable to connect to MySQL1");
 mysqli_select_db($conn,"motaodumusi_db");
 /////////////////////////////////////////////////////////////////
